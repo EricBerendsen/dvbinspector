59568381c6296f7f95ff6d745211262679b398f3
dvbinspector <dvbinspector@82cca20c-c5d6-4b6d-a0f8-b1ac1b36e35b>
2012-08-11 13:29:39 +0000

    delete code, not just comment it out. (Keep it clean!)

--- trunk/src/main/java/nl/digitalekabeltelevisie/util/Utils.java
+++ trunk/src/main/java/nl/digitalekabeltelevisie/util/Utils.java
@@ -923,48 +923,8 @@ public final class Utils {
 			case '>': sb.append("&gt;"); break;
 			case '&': sb.append("&amp;"); break;
 			case '"': sb.append("&quot;"); break;
-			//case '\'':sb.append("&apos;"); break;
 			case '€': sb.append("&euro;"); break;
 			case '™': sb.append("&trade;"); break;
-
-			
-//			case 'à': sb.append("&agrave;");break;
-//			case 'À': sb.append("&Agrave;");break;
-//			case 'â': sb.append("&acirc;");break;
-//			case 'Â': sb.append("&Acirc;");break;
-//			case 'ä': sb.append("&auml;");break;
-//			case 'Ä': sb.append("&Auml;");break;
-//			case 'å': sb.append("&aring;");break;
-//			case 'Å': sb.append("&Aring;");break;
-//			case 'æ': sb.append("&aelig;");break;
-//			case 'Æ': sb.append("&AElig;");break;
-//			case 'ç': sb.append("&ccedil;");break;
-//			case 'Ç': sb.append("&Ccedil;");break;
-//			case 'é': sb.append("&eacute;");break;
-//			case 'É': sb.append("&Eacute;");break;
-//			case 'è': sb.append("&egrave;");break;
-//			case 'È': sb.append("&Egrave;");break;
-//			case 'ê': sb.append("&ecirc;");break;
-//			case 'Ê': sb.append("&Ecirc;");break;
-//			case 'ë': sb.append("&euml;");break;
-//			case 'Ë': sb.append("&Euml;");break;
-//			case 'ï': sb.append("&iuml;");break;
-//			case 'Ï': sb.append("&Iuml;");break;
-//			case 'ô': sb.append("&ocirc;");break;
-//			case 'Ô': sb.append("&Ocirc;");break;
-//			case 'ö': sb.append("&ouml;");break;
-//			case 'Ö': sb.append("&Ouml;");break;
-//			case 'ø': sb.append("&oslash;");break;
-//			case 'Ø': sb.append("&Oslash;");break;
-//			case 'ß': sb.append("&szlig;");break;
-//			case 'ù': sb.append("&ugrave;");break;
-//			case 'Ù': sb.append("&Ugrave;");break;
-//			case 'û': sb.append("&ucirc;");break;
-//			case 'Û': sb.append("&Ucirc;");break;
-//			case 'ü': sb.append("&uuml;");break; //
-//			case 'Ü': sb.append("&Uuml;");break;
-//			 case '®': sb.append("&reg;");break;
-//			 case '©': sb.append("&copy;");break;
 			
 			// extra chars based on http://www.w3schools.com/tags/ref_entities.asp
 			
