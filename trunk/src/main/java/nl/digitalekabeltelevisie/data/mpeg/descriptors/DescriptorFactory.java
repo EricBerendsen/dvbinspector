29db3606ffc7c54bf614bc49afcf146804192549
dvbinspector <dvbinspector@82cca20c-c5d6-4b6d-a0f8-b1ac1b36e35b>
2012-10-31 10:25:57 +0000

    added S2SatelliteDeliverySystemDescriptor

--- trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/DescriptorFactory.java
+++ trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/DescriptorFactory.java
@@ -1,28 +1,28 @@
 /**
- * 
+ *
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
- * 
+ *
  *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
- * 
+ *
  *  This file is part of DVB Inspector.
- * 
+ *
  *  DVB Inspector is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  DVB Inspector is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with DVB Inspector.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  The author requests that he be notified of any application, applet, or
  *  other binary that makes use of this code, but that's more out of curiosity
  *  than anything and is not required.
- * 
+ *
  */
 
 package nl.digitalekabeltelevisie.data.mpeg.descriptors;
@@ -68,7 +68,7 @@ import nl.digitalekabeltelevisie.util.Utils;
 public final class DescriptorFactory {
 
 	/**
-	 * 
+	 *
 	 */
 	private DescriptorFactory() {
 		// static only
@@ -192,8 +192,8 @@ public final class DescriptorFactory {
 				d = new NordigLogicalChannelDescriptorV2(data, t + offset, tableSection);
 				break;
 			}
-			
-			
+
+
 		} else if (private_data_specifier == 0x40) { // CI Plus LLP
 			switch (Utils.getUnsignedByte(data[t + offset])) {
 			case 0xCE:
@@ -397,6 +397,9 @@ public final class DescriptorFactory {
 			break;
 		case 0x77:
 			d = new TimeSliceFecIdentifierDescriptor(data, t + offset, tableSection);
+			break;
+		case 0x79:
+			d = new S2SatelliteDeliverySystemDescriptor(data, t + offset, tableSection);
 			break;
 		case 0x7A:
 			d = new EnhancedAC3Descriptor(data, t + offset, tableSection);
