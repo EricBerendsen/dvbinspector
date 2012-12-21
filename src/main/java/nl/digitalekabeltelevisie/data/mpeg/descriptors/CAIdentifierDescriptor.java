0efcc8281873944e06cd9eb25ae505a08900a1aa
dvbinspector <50879515+EricBerendsen@users.noreply.github.com>
2012-12-21 10:01:24 +0000

    cleanup

--- src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/CAIdentifierDescriptor.java
+++ src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/CAIdentifierDescriptor.java
@@ -1,28 +1,28 @@
 /**
- * 
+ *
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
- * 
+ *
  *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
- * 
+ *
  *  This file is part of DVB Inspector.
- * 
+ *
  *  DVB Inspector is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  DVB Inspector is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with DVB Inspector.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  The author requests that he be notified of any application, applet, or
  *  other binary that makes use of this code, but that's more out of curiosity
  *  than anything and is not required.
- * 
+ *
  */
 
 package nl.digitalekabeltelevisie.data.mpeg.descriptors;
@@ -40,7 +40,7 @@ import nl.digitalekabeltelevisie.data.mpeg.psi.TableSection;
 
 public class CAIdentifierDescriptor extends Descriptor {
 
-	private List<CASystemId> CA_system_id = new ArrayList<CASystemId>();
+	private final List<CASystemId> CA_system_id = new ArrayList<CASystemId>();
 
 	public class CASystemId implements TreeNode{
 		private final int ca_system_id;
@@ -61,7 +61,7 @@ public class CAIdentifierDescriptor extends Descriptor {
 		super(b, offset,parent);
 		int t=0;
 		while(t<descriptorLength){
-			final int caSystemID = getInt(b,offset+2+t,2,0xFFFF);
+			final int caSystemID = getInt(b, offset + 2 + t, 2, MASK_16BITS);
 			final CASystemId caID= new CASystemId(caSystemID);
 			CA_system_id.add(caID);
 			t+=2;
