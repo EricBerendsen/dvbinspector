9f0b12f33f270665ada66a25ea7734d2cdf7c007
dvbinspector <50879515+EricBerendsen@users.noreply.github.com>
2012-12-25 09:08:26 +0000

    activate AC3 and E-AC3 versions

--- src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/EnhancedAC3Descriptor.java
+++ src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/EnhancedAC3Descriptor.java
@@ -1,28 +1,28 @@
 /**
- * 
+ *
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
- * 
+ *
  *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
- * 
+ *
  *  This file is part of DVB Inspector.
- * 
+ *
  *  DVB Inspector is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  DVB Inspector is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with DVB Inspector.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  The author requests that he be notified of any application, applet, or
  *  other binary that makes use of this code, but that's more out of curiosity
  *  than anything and is not required.
- * 
+ *
  */
 
 package nl.digitalekabeltelevisie.data.mpeg.descriptors;
@@ -30,6 +30,7 @@ package nl.digitalekabeltelevisie.data.mpeg.descriptors;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 import nl.digitalekabeltelevisie.controller.KVP;
+import nl.digitalekabeltelevisie.data.mpeg.pes.ac3.AbstractAC3SyncFrame;
 import nl.digitalekabeltelevisie.data.mpeg.psi.TableSection;
 import nl.digitalekabeltelevisie.util.Utils;
 
@@ -111,7 +112,7 @@ public class EnhancedAC3Descriptor extends Descriptor {
 			t.add(new DefaultMutableTreeNode(new KVP("component_type",component_type,AC3Descriptor.getComponentTypeString(component_type))));
 		}
 		if(bsid_flag!=0){
-			t.add(new DefaultMutableTreeNode(new KVP("bsid",bsid,"Enhanced AC-3 coding version")));
+			t.add(new DefaultMutableTreeNode(new KVP("bsid",bsid,AbstractAC3SyncFrame.getBsidString(bsid))));
 		}
 		if(mainid_flag!=0){
 			t.add(new DefaultMutableTreeNode(new KVP("mainid",mainid,null)));
