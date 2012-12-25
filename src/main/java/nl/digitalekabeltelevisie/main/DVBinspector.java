4fd79d443d958d3ba81600cf3c75c6bcbe91806f
dvbinspector <50879515+EricBerendsen@users.noreply.github.com>
2012-12-25 09:05:15 +0000

    added PTS option

--- src/main/java/nl/digitalekabeltelevisie/main/DVBinspector.java
+++ src/main/java/nl/digitalekabeltelevisie/main/DVBinspector.java
@@ -1,28 +1,28 @@
 /**
- * 
+ *
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
- * 
+ *
  *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
- * 
+ *
  *  This file is part of DVB Inspector.
- * 
+ *
  *  DVB Inspector is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  DVB Inspector is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with DVB Inspector.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  The author requests that he be notified of any application, applet, or
  *  other binary that makes use of this code, but that's more out of curiosity
  *  than anything and is not required.
- * 
+ *
  */
 package nl.digitalekabeltelevisie.main;
 
@@ -131,12 +131,12 @@ public class DVBinspector implements ChangeListener{
 			                pesHandlerMap.put(Integer.valueOf(p.getPid()), p.getPesHandler());
 			            }
 			        }
-			        ts.parseStream(pesHandlerMap);					
+			        ts.parseStream(pesHandlerMap);
 				}
 			} catch (final IOException e) {
 				logger.log(Level.WARNING, "error parsing transportStream", e);
 			}
-			
+
 
 }
 
@@ -267,6 +267,13 @@ public class DVBinspector implements ChangeListener{
 		countListViewMenu.addActionListener(countListViewAction);
 		viewTreeMenu.add(countListViewMenu);
 
+		final JCheckBoxMenuItem showPtsViewMenu = new JCheckBoxMenuItem("Show PTS on PES Packets");
+		showPtsViewMenu.setSelected((modus&DVBtree.SHOW_PTS_MODUS)!=0);
+		final Action showPtsViewAction= new ToggleViewAction(this, DVBtree.SHOW_PTS_MODUS);
+		showPtsViewMenu.addActionListener(showPtsViewAction);
+		viewTreeMenu.add(showPtsViewMenu);
+
+
 		privateDataSubMenu = new JMenu("Private Data Specifier Default");
 		final ButtonGroup group = new ButtonGroup();
 
@@ -389,7 +396,7 @@ public class DVBinspector implements ChangeListener{
 
 	/* (non-Javadoc)
 	 * @see javax.swing.event.ChangeListener#stateChanged(javax.swing.event.ChangeEvent)
-	 * 
+	 *
 	 * Listen for changes caused by selecting another tab from tabbedPane. Then enable/disable appropriate menu's.
 	 */
 
@@ -398,7 +405,7 @@ public class DVBinspector implements ChangeListener{
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	private void enableViewMenus() {
 		final int i = tabbedPane.getSelectedIndex();
