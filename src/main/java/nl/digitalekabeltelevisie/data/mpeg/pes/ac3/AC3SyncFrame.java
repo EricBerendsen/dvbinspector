077bdb2f611d240a04904e4521a54527ab120a8a
dvbinspector <50879515+EricBerendsen@users.noreply.github.com>
2012-12-30 14:48:23 +0000

    AC3 and E-AC3 versions finished

--- src/main/java/nl/digitalekabeltelevisie/data/mpeg/pes/ac3/AC3SyncFrame.java
+++ src/main/java/nl/digitalekabeltelevisie/data/mpeg/pes/ac3/AC3SyncFrame.java
@@ -40,49 +40,12 @@ import nl.digitalekabeltelevisie.controller.TreeNode;
  */
 public class AC3SyncFrame extends AbstractAC3SyncFrame implements TreeNode {
 
-	private final  int crc1;
-	private final  int frmsizecod;
-
-	private final  int acmod;
-	private int cmixlev;
-	private int surmixlev;
-	private final  int langcode;
-	private int langcod;
-	private int langcod2e;
-	private int langcod2;
-	private int timecod1e;
-	private int timecod1;
-	private int timecod2e;
-	private int timecod2;
-
 	// Alternate bit stream syntax
 	private int xbsi1e;
 	private int xbsi2e;
-	private int dsurexmod;
-	private int adconvtyp;
 	private int xbsi2;
 	private int encinfo;
 
-	private final int addbsie;
-	private int addbsil;
-
-	// based on  ProjectX AudioFormatAC3.java
-	static int[] ac3_bitrate_index =  {
-			32000, 40000, 48000, 56000, 64000, 80000, 96000,
-			112000, 128000, 160000, 192000, 224000, 256000,
-			320000, 384000, 448000, 512000, 576000, 640000,
-			0,0,0,0,0,0,0,0,0,0,0,0,0
-		};
-
-	// this is different from the version in ProjectX in 2 ways;
-	// 1) it has size in words (2 bytes) instead of bytes (more confirming to the spec)
-	// 2) double number of entries, for 44,1 kHz this makes difference
-
-	private static int[][] ac3_size_table = {
-		{64,64,80,80,96,96,112,112,128,128,160,160,192,192,224,224,256,256,320,320,384,384,448,448,512,512,640,640,768,768,896,896,1024,1024,1152,1152,1280,1280}, //48 kHz
-		{69,70,87,88,104,105,121,122,139,140,174,175,208,209,243,244,278,279,348,349,417,418,487,488,557,558,696,697,836,835,975,976,1114,1115,1253,1254,1393,1394}, // 44,1 kHz
-		{96,96,120,120,144,144,168,168,192,192,240,240,288,288,336,336,384,384,480,480,576,576,672,672,768,768,960,960,1152,1152,1344,1344,1560,1560,1728,1728,1920,1920} //32 kHz
-	};
 
 	public AC3SyncFrame(final byte[] data, final int offset) {
 		super(data,offset);
@@ -194,7 +157,7 @@ public class AC3SyncFrame extends AbstractAC3SyncFrame implements TreeNode {
 			s.add(new DefaultMutableTreeNode(new KVP("surmixlev",surmixlev,"Surround mix level: "+getSurroundMixLevelString(surmixlev))));
 		}
 		if(acmod == 0x2){ /* if in 2/0 mode */
-			s.add(new DefaultMutableTreeNode(new KVP("dsurmod",dsurmod,"Dolby Surround mode")));
+			s.add(new DefaultMutableTreeNode(new KVP("dsurmod",dsurmod,"Dolby Surround mode: "+getDsurmodString(dsurmod))));
 		}
 		s.add(new DefaultMutableTreeNode(new KVP("lfeon",lfeon,getLfeOnString(lfeon))));
 
@@ -210,7 +173,7 @@ public class AC3SyncFrame extends AbstractAC3SyncFrame implements TreeNode {
 		s.add(new DefaultMutableTreeNode(new KVP("audprodie",audprodie,audprodie==1?"mixlevel and roomtyp fields exist":"No audio production information exists")));
 		if(audprodie==1){
 			s.add(new DefaultMutableTreeNode(new KVP("mixlevel",mixlevel,"Mixing level: "+(80+mixlevel)+" dB")));
-			s.add(new DefaultMutableTreeNode(new KVP("roomtyp","Room type: "+roomtyp,AC3SyncFrame.getRoomTypeString(roomtyp))));
+			s.add(new DefaultMutableTreeNode(new KVP("roomtyp","Room type: "+roomtyp,getRoomTypeString(roomtyp))));
 		}
 		if(acmod == 0){ /* if 1+1 mode (dual mono, so some items need a second value) */
 			s.add(new DefaultMutableTreeNode(new KVP("dialnorm2",dialnorm2,getDialNormString(dialnorm2))));
@@ -225,7 +188,7 @@ public class AC3SyncFrame extends AbstractAC3SyncFrame implements TreeNode {
 			s.add(new DefaultMutableTreeNode(new KVP("audprodi2e",audprodi2e,audprodi2e==1?"following two data fields exist indicating information about the audio production for Ch2":"No audio production information exists for Ch2")));
 			if(audprodi2e==1){
 				s.add(new DefaultMutableTreeNode(new KVP("mixlevel2",mixlevel2,"peak mixing level during the final audio mixing session: "+(80+mixlevel2)+" dB")));
-				s.add(new DefaultMutableTreeNode(new KVP("roomtyp2",roomtyp2,AC3SyncFrame.getRoomTypeString(roomtyp))));
+				s.add(new DefaultMutableTreeNode(new KVP("roomtyp2",roomtyp2,getRoomTypeString(roomtyp))));
 			}
 		}
 
@@ -244,7 +207,7 @@ public class AC3SyncFrame extends AbstractAC3SyncFrame implements TreeNode {
 		}else{  // Alternate bit stream syntax
 			s.add(new DefaultMutableTreeNode(new KVP("xbsi1e",xbsi1e,"Extra bitstream information #1 exists")));
 			if(xbsi1e==1){
-				s.add(new DefaultMutableTreeNode(new KVP("dmixmod",dmixmod,"Preferred stereo downmix mode: "+AC3SyncFrame.getDmixmodString(dmixmod))));
+				s.add(new DefaultMutableTreeNode(new KVP("dmixmod",dmixmod,"Preferred stereo downmix mode: "+getDmixmodString(dmixmod))));
 				s.add(new DefaultMutableTreeNode(new KVP("ltrtcmixlev",ltrtcmixlev,"Lt/Rt center mix level: "+getCmixlevString(ltrtcmixlev))));
 				s.add(new DefaultMutableTreeNode(new KVP("ltrtsurmixlev",ltrtsurmixlev,"Lt/Rt surround mix level: "+getSurmixlevString(ltrtsurmixlev))));
 				s.add(new DefaultMutableTreeNode(new KVP("lorocmixlev",lorocmixlev,"Lo/Ro center mix level: "+getCmixlevString(lorocmixlev))));
@@ -269,213 +232,13 @@ public class AC3SyncFrame extends AbstractAC3SyncFrame implements TreeNode {
 	}
 
 
-	private String getDsurexmodString(final int dsurexmod) {
-		final String [] desc = {"Not indicated",
-				"Not Dolby Surround EX encoded",
-				"Dolby Surround EX encoded",
-				"Reserved"
-				};
-		return desc[dsurexmod];
-	}
-
-	private String getDheadphonmodString(final int dheadphonmod) {
-		final String [] desc = {"Not indicated",
-				"Not Dolby Headphone encoded",
-				"Dolby Headphone encoded",
-				"Reserved"
-				};
-		return desc[dheadphonmod];
-	}
-
-
-	private String getSurmixlevString(final int surmixlev) {
-		final String [] desc = {"Reserved",
-				"Reserved",
-				"Reserved",
-				"0,841 (-1,5 dB)",
-				"0,707 (-3,0 dB)",
-				"0,595 (-4,5 dB)",
-				"0,500 (-6,0 dB)",
-				"0,000 (-inf dB)"};
-		return desc[surmixlev];
-	}
-
-
-	private String getCmixlevString(final int cmixlev) {
-		final String [] desc = {"1,414 (+3,0 dB)",
-				"1,189 (+1,5 dB)",
-				"1,000 (0,0 dB)",
-				"0,841 (-1,5 dB)",
-				"0,707 (-3,0 dB)",
-				"0,595 (-4,5 dB)",
-				"0,500 (-6,0 dB)",
-				"0,000 (-inf dB)"};
-		return desc[cmixlev];
-	}
-
-
-	public int getOffset() {
-		return offset;
-	}
-
-
-	public int getSyncword() {
-		return syncword;
-	}
-
-
-	public int getCrc1() {
-		return crc1;
-	}
-
-
-	public int getFscod() {
-		return fscod;
-	}
-
-
-	public int getFrmsizecod() {
-		return frmsizecod;
-	}
-
-
-	public int getBsid() {
-		return bsid;
-	}
-
-
-	public int getAcmod() {
-		return acmod;
-	}
-
-
-	public int getCmixlev() {
-		return cmixlev;
-	}
-
-
-	public int getSurmixlev() {
-		return surmixlev;
-	}
-
-
-	public int getLfeon() {
-		return lfeon;
-	}
-
-
-	public int getDialnorm() {
-		return dialnorm;
-	}
-
-
-	public int getLangcode() {
-		return langcode;
-	}
-
-
-	public int getLangcod() {
-		return langcod;
-	}
-
-
-	public int getAudprodie() {
-		return audprodie;
-	}
-
-
-	public int getMixlevel() {
-		return mixlevel;
-	}
-
-
-	public int getRoomtyp() {
-		return roomtyp;
-	}
-
-
-	public int getDialnorm2() {
-		return dialnorm2;
-	}
-
-
-	public int getLangcod2e() {
-		return langcod2e;
-	}
-
-
-	public int getLangcod2() {
-		return langcod2;
-	}
-
-
-	public int getAudprodi2e() {
-		return audprodi2e;
-	}
-
-
-	public int getMixlevel2() {
-		return mixlevel2;
-	}
-
-
-	public int getRoomtyp2() {
-		return roomtyp2;
-	}
-
-
-	public int getCopyrightb() {
-		return copyrightb;
-	}
-
-
-	public int getTimecod1e() {
-		return timecod1e;
-	}
-
-
-	public int getTimecod1() {
-		return timecod1;
-	}
-
-
-	public int getTimecod2e() {
-		return timecod2e;
-	}
-
-
-	public int getTimecod2() {
-		return timecod2;
-	}
-
-
 	public int getXbsi1e() {
 		return xbsi1e;
 	}
 
 
-	public int getDmixmod() {
-		return dmixmod;
-	}
-
-
 	public int getXbsi2e() {
 		return xbsi2e;
-	}
-
-
-	public int getDsurexmod() {
-		return dsurexmod;
-	}
-
-
-	public int getDheadphonmod() {
-		return dheadphonmod;
-	}
-
-
-	public int getAdconvtyp() {
-		return adconvtyp;
 	}
 
 
@@ -486,108 +249,6 @@ public class AC3SyncFrame extends AbstractAC3SyncFrame implements TreeNode {
 
 	public int getEncinfo() {
 		return encinfo;
-	}
-
-
-	public int getAddbsie() {
-		return addbsie;
-	}
-
-
-	public int getAddbsil() {
-		return addbsil;
-	}
-
-
-	public static String getFrmsizecodString(final int fscod, final int frmsizecod){
-		if((fscod>=0) && (fscod<3) && (frmsizecod>=0) && (frmsizecod <=37)){
-			return "Nominal bit rate: "+ac3_bitrate_index[frmsizecod>>>1]+", words/syncframe: "+ac3_size_table[fscod][frmsizecod];
-		}else{
-			return "illegal values for fscod: "+fscod+" or frmsizecod: "+frmsizecod;
-		}
-
-	}
-
-
-	public static String getBsModString(final int bsmod, final int acmod){
-		switch (bsmod) {
-		case 0:
-			return "Main audio service: complete main (CM)";
-		case 1:
-			return "Main audio service: music and effects (ME)";
-		case 2:
-			return "Associated service: visually impaired (VI)";
-		case 3:
-			return "Associated service: hearing impaired (HI)";
-		case 4:
-			return "Associated service: dialogue (D)";
-		case 5:
-			return "Associated service: commentary (C)";
-		case 6:
-			return "Associated service: emergency (E)";
-		case 7:
-			if(acmod==1){
-				return "Associated service: dialogue (D)";
-			}else if((acmod>=2) && (acmod<=7)){
-				return "Main audio service: karaoke";
-			}else{
-				return "Illegal value for acmod: "+acmod;
-			}
-
-		default:
-			return "Illegal Value for bsmod: "+bsmod;
-		}
-	}
-
-
-	public static String getCentreMixLevelString(final int cmixlev){
-		switch (cmixlev) {
-		case 0:
-			return "0,707 (-3,0 dB)";
-		case 1:
-			return "0,595 (-4,5 dB)";
-		case 2:
-			return "0,500 (-6,0 dB)";
-		case 3:
-			return "reserved";
-		default:
-			return "Illegal value";
-		}
-	}
-
-
-	public static String getSurroundMixLevelString(final int surmixlev){
-		switch (surmixlev) {
-		case 0:
-			return "0,707 (-3,0 dB)";
-		case 1:
-			return "0,500 (-6 dB)";
-		case 2:
-			return "0";
-		case 3:
-			return "reserved";
-		default:
-			return "Illegal value";
-		}
-	}
-
-
-	public static String getRoomTypeString(final int roomtyp){
-
-		switch (roomtyp) {
-
-		case 0:
-			return "Not indicated";
-		case 1:
-			return "Large room, X curve monitor";
-		case 2:
-			return "Small room, flat monitor";
-		case 3:
-			return "reserved";
-
-		default:
-			return "Illegal value";
-		}
 	}
 
 
