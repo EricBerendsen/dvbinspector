553c76174875f7cce36a62128e673708497a3fee
dvbinspector <50879515+EricBerendsen@users.noreply.github.com>
2013-01-03 19:51:18 +0000

    cleanup dead code

--- src/main/java/nl/digitalekabeltelevisie/gui/PIDDialogOpenAction.java
+++ src/main/java/nl/digitalekabeltelevisie/gui/PIDDialogOpenAction.java
@@ -1,28 +1,28 @@
 /**
- * 
+ *
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
- * 
+ *
  *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
- * 
+ *
  *  This file is part of DVB Inspector.
- * 
+ *
  *  DVB Inspector is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  DVB Inspector is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with DVB Inspector.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  The author requests that he be notified of any application, applet, or
  *  other binary that makes use of this code, but that's more out of curiosity
  *  than anything and is not required.
- * 
+ *
  */
 
 package nl.digitalekabeltelevisie.gui;
@@ -36,18 +36,16 @@ import nl.digitalekabeltelevisie.main.DVBinspector;
 
 public class PIDDialogOpenAction extends AbstractAction {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 196484992076713295L;
 	private final PIDDialog pIDDialog;
 	private final JFrame f;
-	private final DVBinspector contr;
 
 	public PIDDialogOpenAction(final PIDDialog pd, final JFrame fr, final DVBinspector controller) {
 		super();
 		pIDDialog=pd;
 		f=fr;
-		contr=controller;
 	}
 	public void actionPerformed(final ActionEvent e) {
 
