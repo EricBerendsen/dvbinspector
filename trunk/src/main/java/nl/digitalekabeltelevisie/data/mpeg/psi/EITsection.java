f69bf4555bf44061645edaf2114fb0c1db14ae5e
dvbinspector <dvbinspector@82cca20c-c5d6-4b6d-a0f8-b1ac1b36e35b>
2013-01-30 20:43:18 +0000

    Show EIT in image (grid), per table type (actual/schedule * current/other)

--- trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/psi/EITsection.java
+++ trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/psi/EITsection.java
@@ -1,29 +1,29 @@
 package nl.digitalekabeltelevisie.data.mpeg.psi;
 /**
- * 
+ *
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
- * 
+ *
  *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
- * 
+ *
  *  This file is part of DVB Inspector.
- * 
+ *
  *  DVB Inspector is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  DVB Inspector is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with DVB Inspector.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  The author requests that he be notified of any application, applet, or
  *  other binary that makes use of this code, but that's more out of curiosity
  *  than anything and is not required.
- * 
+ *
  */
 
 import java.util.ArrayList;
@@ -92,25 +92,23 @@ public class EITsection extends TableSection implements HTMLSource {
 
 		}
 
-		private String getEventName(){
+		public String getEventName(){
 
-			String r=null;
 
 			final Iterator<Descriptor> descs=descriptorList.iterator();
 			while(descs.hasNext()){
 				final Descriptor d=descs.next();
 				if(d instanceof ShortEventDescriptor) {
-					r = ((ShortEventDescriptor)d).getEventName().toString();
-					return " "+r;
+					return ((ShortEventDescriptor)d).getEventName().toString();
 				}
 			}
 			//  no ShortEventDescriptor, give up
-			return r;
+			return "";
 
 		}
 		public DefaultMutableTreeNode getJTreeNode(final int modus){
 
-			final DefaultMutableTreeNode t = new DefaultMutableTreeNode(new KVP("event",eventID,Utils.getUTCFormattedString(startTime)+getEventName()));
+			final DefaultMutableTreeNode t = new DefaultMutableTreeNode(new KVP("event",eventID,Utils.getUTCFormattedString(startTime)+" "+getEventName()));
 
 			t.add(new DefaultMutableTreeNode(new KVP("event_id",eventID,null)));
 			t.add(new DefaultMutableTreeNode(new KVP("start_time",startTime,Utils.getUTCFormattedString(startTime))));
