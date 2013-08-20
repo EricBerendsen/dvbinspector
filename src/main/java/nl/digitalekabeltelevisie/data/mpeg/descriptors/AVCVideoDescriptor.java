b367e6558e98a717a51472533ae195839c84bbf1
dvbinspector <50879515+EricBerendsen@users.noreply.github.com>
2013-08-20 11:12:15 +0000

    added description for profile_idc

--- src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/AVCVideoDescriptor.java
+++ src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/AVCVideoDescriptor.java
@@ -30,9 +30,16 @@ package nl.digitalekabeltelevisie.data.mpeg.descriptors;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 import nl.digitalekabeltelevisie.controller.KVP;
+import nl.digitalekabeltelevisie.data.mpeg.pes.video264.Seq_parameter_set_rbsp;
 import nl.digitalekabeltelevisie.data.mpeg.psi.TableSection;
 import nl.digitalekabeltelevisie.util.Utils;
 
+/**
+ *
+ * Based on 2.6.64 AVC video descriptor, ISO/IEC 13818-1:2013 / Rec. ITU-T H.222.0 (06/2012)
+ * @author Eric
+ *
+ */
 public class AVCVideoDescriptor extends Descriptor {
 
 
@@ -73,7 +80,7 @@ public class AVCVideoDescriptor extends Descriptor {
 	public DefaultMutableTreeNode getJTreeNode(final int modus){
 
 		final DefaultMutableTreeNode t = super.getJTreeNode(modus);
-		t.add(new DefaultMutableTreeNode(new KVP("profile_idc",profile_idc,null)));
+		t.add(new DefaultMutableTreeNode(new KVP("profile_idc",profile_idc,Seq_parameter_set_rbsp.getProfileIdcString(profile_idc))));
 		t.add(new DefaultMutableTreeNode(new KVP("constraint_set0_flag",constraint_set0_flag,null)));
 		t.add(new DefaultMutableTreeNode(new KVP("constraint_set1_flag",constraint_set1_flag,null)));
 		t.add(new DefaultMutableTreeNode(new KVP("constraint_set2_flag",constraint_set2_flag,null)));
