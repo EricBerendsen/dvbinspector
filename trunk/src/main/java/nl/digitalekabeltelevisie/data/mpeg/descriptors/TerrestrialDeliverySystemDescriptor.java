ab7397dad3bcd189d87c6ad653e409aa2ee39eee
dvbinspector <dvbinspector@82cca20c-c5d6-4b6d-a0f8-b1ac1b36e35b>
2013-09-07 13:16:52 +0000

    added getters

--- trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/TerrestrialDeliverySystemDescriptor.java
+++ trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/TerrestrialDeliverySystemDescriptor.java
@@ -1,28 +1,28 @@
 /**
- * 
+ *
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
- * 
+ *
  *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
- * 
+ *
  *  This file is part of DVB Inspector.
- * 
+ *
  *  DVB Inspector is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  DVB Inspector is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with DVB Inspector.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  The author requests that he be notified of any application, applet, or
  *  other binary that makes use of this code, but that's more out of curiosity
  *  than anything and is not required.
- * 
+ *
  */
 
 package nl.digitalekabeltelevisie.data.mpeg.descriptors;
@@ -68,7 +68,7 @@ public class TerrestrialDeliverySystemDescriptor extends Descriptor {
 
 
 
-	private static String getBandwidtString(final int b) {
+	public static String getBandwidtString(final int b) {
 		switch (b) {
 		case 0: return "8 MHz";
 		case 1: return "7 MHz";
@@ -133,5 +133,20 @@ public class TerrestrialDeliverySystemDescriptor extends Descriptor {
 		t.add(new DefaultMutableTreeNode(new KVP("FEC_inner",FEC_inner ,getFEC_innerString())));
 
 		return t;
+	}
+
+
+	public int getBandwidth() {
+		return bandwidth;
+	}
+
+
+	public int getPriority() {
+		return priority;
+	}
+
+
+	public int getTime_Slicing_indicator() {
+		return time_Slicing_indicator;
 	}
 }
