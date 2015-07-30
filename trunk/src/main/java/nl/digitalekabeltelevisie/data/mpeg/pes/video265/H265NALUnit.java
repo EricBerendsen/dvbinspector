de0345798039bbd51351902e75a749ea4b2cb92a
dvbinspector <dvbinspector@82cca20c-c5d6-4b6d-a0f8-b1ac1b36e35b>
2015-07-30 15:12:37 +0000

    Added Pic_parameter_set_rbsp, made NALUnitType an enum.

--- trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/pes/video265/H265NALUnit.java
+++ trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/pes/video265/H265NALUnit.java
@@ -2,7 +2,7 @@
  * 
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
  * 
- *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
+ *  This code is Copyright 2009-2015 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
  * 
  *  This file is part of DVB Inspector.
  * 
@@ -35,7 +35,7 @@ import nl.digitalekabeltelevisie.data.mpeg.pes.video26x.*;
 public class H265NALUnit extends AbstractNALUnit implements TreeNode {
 
 	private final int forbidden_zero_bit;
-	private final int nal_unit_type;
+	private final NALUnitType nal_unit_type;
 	private final int nuh_layer_id;
 	private final int nuh_temporal_id_plus1;
 
@@ -49,7 +49,7 @@ public class H265NALUnit extends AbstractNALUnit implements TreeNode {
 		super(bytes, offset, len);
 
 		this.forbidden_zero_bit = bs.readBits(1);
-		this.nal_unit_type = bs.readBits(6);
+		this.nal_unit_type = NALUnitType.getByType(bs.readBits(6));
 		this.nuh_layer_id = bs.readBits(6);
 		this.nuh_temporal_id_plus1 = bs.readBits(3);
 
@@ -58,11 +58,11 @@ public class H265NALUnit extends AbstractNALUnit implements TreeNode {
 	}
 
 	public DefaultMutableTreeNode getJTreeNode(final int modus) {
-		final DefaultMutableTreeNode t = new DefaultMutableTreeNode(new KVP("NALUnit ("+getNALUnitTypeString(nal_unit_type)+")"));
+		final DefaultMutableTreeNode t = new DefaultMutableTreeNode(new KVP("NALUnit ("+nal_unit_type.name()+" "+ nal_unit_type.getDescription())+")");
 		t.add(new DefaultMutableTreeNode(new KVP("bytes",bytes,offset,numBytesInNALunit,null)));
 		t.add(new DefaultMutableTreeNode(new KVP("numBytesInNALunit",numBytesInNALunit,null)));
 		t.add(new DefaultMutableTreeNode(new KVP("forbidden_zero_bit",forbidden_zero_bit,null)));
-		t.add(new DefaultMutableTreeNode(new KVP("nal_unit_type",nal_unit_type,getNALUnitTypeString(nal_unit_type))));
+		t.add(new DefaultMutableTreeNode(new KVP("nal_unit_type",nal_unit_type.getType(),nal_unit_type.getDescription())));
 		t.add(new DefaultMutableTreeNode(new KVP("nuh_layer_id",nuh_layer_id,null)));
 		t.add(new DefaultMutableTreeNode(new KVP("nuh_temporal_id_plus1",nuh_temporal_id_plus1,null)));
 
@@ -74,67 +74,79 @@ public class H265NALUnit extends AbstractNALUnit implements TreeNode {
 		return t;
 	}
 
-
 	public String getNALUnitTypeString(final int nal_unit_type) {
-
-		switch (nal_unit_type) {
-		case 0: return "TRAIL_N Coded slice segment of a non-TSA, non-STSA trailing picture";
-		case 1: return "TRAIL_R Coded slice segment of a non-TSA, non-STSA trailing picture";
-		case 2 : return "TSA_N Coded slice segment of a TSA picture";
-		case 3 : return "TSA_R Coded slice segment of a TSA picture";
-		case 4 : return "STSA_N Coded slice segment of an STSA picture";
-		case 5 : return "STSA_R Coded slice segment of an STSA picture";
-		case 6 : return "RADL_N Coded slice segment of a RADL picture";
-		case 7 : return "RADL_R Coded slice segment of a RADL picture";
-		case 8 : return "RASL_N Coded slice segment of a RASL picture";
-		case 9 : return "RASL_R Coded slice segment of a RASL picture";
-		case 10 : return "RSV_VCL_N10 Reserved non-IRAP SLNR VCL NAL unit types";
-		case 11 : return "RSV_VCL_R11 Reserved non-IRAP sub-layer reference VCL NAL unit types";
-		case 12 : return "RSV_VCL_N12 Reserved non-IRAP SLNR VCL NAL unit types";
-		case 13 : return "RSV_VCL_R13 Reserved non-IRAP sub-layer reference VCL NAL unit types";
-		case 14 : return "RSV_VCL_N14 Reserved non-IRAP SLNR VCL NAL unit types";
-		case 15 : return "RSV_VCL_R15 Reserved non-IRAP sub-layer reference VCL NAL unit types";
-		case 16 : return "BLA_W_LP Coded slice segment of a BLA picture";
-		case 17 : return "BLA_W_RADL Coded slice segment of a BLA picture";
-		case 18 : return "BLA_N_LP Coded slice segment of a BLA picture";
-		case 19 : return "IDR_W_RADL Coded slice segment of an IDR picture";
-		case 20 : return "IDR_N_LP Coded slice segment of an IDR picture";
-		case 21 : return "CRA_NUT Coded slice segment of a CRA picture";
-		case 22 : return "RSV_IRAP_VCL22 Reserved IRAP VCL NAL unit types";
-		case 23 : return "RSV_IRAP_VCL23 Reserved IRAP VCL NAL unit types";
-		case 32 : return "VPS_NUT Video parameter set";
-		case 33 : return "SPS_NUT Sequence parameter set";
-
-		case 34 : return "PPS_NUT Picture parameter set";
-		case 35 : return "AUD_NUT Access unit delimiter";
-
-		case 36 : return "EOS_NUT End of sequence";
-		case 37 : return "EOB_NUT End of bitstream";
-		case 38 : return "FD_NUT Filler data";
-		case 39 : return "PREFIX_SEI_NUT Supplemental enhancement information";
-		case 40 : return "SUFFIX_SEI_NUT Supplemental enhancement information";
-
-		default:
-			return "reserved";
-		}
+		return NALUnitType.getDescription(nal_unit_type);
 	}
 
+	//	public String getNALUnitTypeString(final int nal_unit_type) {
+	//
+	//		switch (nal_unit_type) {
+	//		case 0: return "TRAIL_N Coded slice segment of a non-TSA, non-STSA trailing picture";
+	//		case 1: return "TRAIL_R Coded slice segment of a non-TSA, non-STSA trailing picture";
+	//		case 2 : return "TSA_N Coded slice segment of a TSA picture";
+	//		case 3 : return "TSA_R Coded slice segment of a TSA picture";
+	//		case 4 : return "STSA_N Coded slice segment of an STSA picture";
+	//		case 5 : return "STSA_R Coded slice segment of an STSA picture";
+	//		case 6 : return "RADL_N Coded slice segment of a RADL picture";
+	//		case 7 : return "RADL_R Coded slice segment of a RADL picture";
+	//		case 8 : return "RASL_N Coded slice segment of a RASL picture";
+	//		case 9 : return "RASL_R Coded slice segment of a RASL picture";
+	//		case 10 : return "RSV_VCL_N10 Reserved non-IRAP SLNR VCL NAL unit types";
+	//		case 11 : return "RSV_VCL_R11 Reserved non-IRAP sub-layer reference VCL NAL unit types";
+	//		case 12 : return "RSV_VCL_N12 Reserved non-IRAP SLNR VCL NAL unit types";
+	//		case 13 : return "RSV_VCL_R13 Reserved non-IRAP sub-layer reference VCL NAL unit types";
+	//		case 14 : return "RSV_VCL_N14 Reserved non-IRAP SLNR VCL NAL unit types";
+	//		case 15 : return "RSV_VCL_R15 Reserved non-IRAP sub-layer reference VCL NAL unit types";
+	//		case 16 : return "BLA_W_LP Coded slice segment of a BLA picture";
+	//		case 17 : return "BLA_W_RADL Coded slice segment of a BLA picture";
+	//		case 18 : return "BLA_N_LP Coded slice segment of a BLA picture";
+	//		case 19 : return "IDR_W_RADL Coded slice segment of an IDR picture";
+	//		case 20 : return "IDR_N_LP Coded slice segment of an IDR picture";
+	//		case 21 : return "CRA_NUT Coded slice segment of a CRA picture";
+	//		case 22 : return "RSV_IRAP_VCL22 Reserved IRAP VCL NAL unit types";
+	//		case 23 : return "RSV_IRAP_VCL23 Reserved IRAP VCL NAL unit types";
+	//		case 32 : return "VPS_NUT Video parameter set";
+	//		case 33 : return "SPS_NUT Sequence parameter set";
+	//
+	//		case 34 : return "PPS_NUT Picture parameter set";
+	//		case 35 : return "AUD_NUT Access unit delimiter";
+	//
+	//		case 36 : return "EOS_NUT End of sequence";
+	//		case 37 : return "EOB_NUT End of bitstream";
+	//		case 38 : return "FD_NUT Filler data";
+	//		case 39 : return "PREFIX_SEI_NUT Supplemental enhancement information";
+	//		case 40 : return "SUFFIX_SEI_NUT Supplemental enhancement information";
+	//
+	//		default:
+	//			return "reserved";
+	//		}
+	//	}
+	//
 
 	/* (non-Javadoc)
 	 * @see nl.digitalekabeltelevisie.data.mpeg.pes.video26x.AbstractNALUnit#createRBSP()
 	 */
 	@Override
 	protected void createRBSP() {
-		if(nal_unit_type==32){
+		switch(nal_unit_type){
+		case VPS_NUT:
 			rbsp=new Video_parameter_set_rbsp(rbsp_byte, numBytesInRBSP);
-		}else if(nal_unit_type==33){
+			break;
+		case SPS_NUT:
 			rbsp=new Seq_parameter_set_rbsp(rbsp_byte, numBytesInRBSP);
-		}else if(nal_unit_type==35){
+			break;
+		case PPS_NUT:
+			rbsp=new Pic_parameter_set_rbsp(rbsp_byte, numBytesInRBSP);
+			break;
+		case AUD_NUT:
 			rbsp=new Access_unit_delimiter_rbsp(rbsp_byte, numBytesInRBSP);
-		}else if(nal_unit_type==39){
+			break;
+		case PREFIX_SEI_NUT:
+		case SUFFIX_SEI_NUT:
 			rbsp=new Sei_rbsp(rbsp_byte, numBytesInRBSP);
-		}else{
-			//logger.warning("not implemented nal_unit_type: "+nal_unit_type);
+			break;
+		default:
+			logger.info("unimplemented nal_unit_type:" +nal_unit_type.getType() + ", " + nal_unit_type.getDescription());
 		}
 	}
 
