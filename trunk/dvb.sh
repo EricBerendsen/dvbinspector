11ea3eab75ce539d2b7834505a831641cf5ef0e7
dvbinspector <dvbinspector@82cca20c-c5d6-4b6d-a0f8-b1ac1b36e35b>
2016-01-17 12:11:17 +0000

    prepare for release 1.6.0

---
+++ trunk/dvb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -verbose -classpath .:./lib/jfreechart-1.0.13.jar:./lib/jcommon-1.0.16.jar:./lib/opencsv-2.1.jar:./lib/jlayer-1.0.1-1.jar:./DVBinspector.jar -Djava.util.logging.config.file=src/main/resources/logging.properties nl.digitalekabeltelevisie.main.DVBinspector
