3d113efdfa7dac338c38d38502bc446631e65a37
dvbinspector <dvbinspector@82cca20c-c5d6-4b6d-a0f8-b1ac1b36e35b>
2018-01-18 21:29:15 +0000

    Rec. ITU-T H.222.0 (2014)/Amd.7 (03/2017) – Prepublished version

--- trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/extension/mpeg/MPEGExtensionDescriptor.java
+++ trunk/src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/extension/mpeg/MPEGExtensionDescriptor.java
@@ -22,6 +22,30 @@ public class MPEGExtensionDescriptor extends ExtensionDescriptor {
 			return "HEVC_timing_and_HRD_descriptor";
 		case 0x4:
 			return "af_extensions_descriptor";
+		case 0x5:
+			return "HEVC_operation_point_descriptor";
+		case 0x6:
+			return "HEVC_hierarchy_extension_descriptor";
+		case 0x7:
+			return "Green_extension_descriptor";
+		case 0x8:
+			return "MPEG-H_3dAudio_descriptor";
+		case 0x9:
+			return "MPEG-H_3dAudio_config_descriptor";
+		case 0xa:
+			return "MPEG-H_3dAudio_scene_descriptor";
+		case 0xb:
+			return "MPEG-H_3dAudio_text_label_descriptor";
+		case 0xc:
+			return "MPEG-H_3dAudio_multi-stream_descriptor";
+		case 0xd:
+			return "MPEG--H_3dAudio_drc_loudness_descriptor";
+		case 0xe:
+			return "MPEG--H_3dAudio_command_descriptor";
+		case 0xf:
+			return "MPEG-Quality_extension_descriptor";
+		case 0x10:
+			return "MPEG-Virtual_segmentation_descriptor";
 		default:
 			return "ITU-T H.222.0 | ISO/IEC 13818-1 Reserved";
 		}
