571bde767bcb8c52ff878c11bbab932eb91df41f
dvbinspector <dvbinspector@82cca20c-c5d6-4b6d-a0f8-b1ac1b36e35b>
2019-02-01 21:15:05 +0000

    added WrapLayout to EITView and GridView, so toolbars with buttons remain visible and usable when window is resized to very small

--- trunk/src/main/java/nl/digitalekabeltelevisie/gui/GridView.java
+++ trunk/src/main/java/nl/digitalekabeltelevisie/gui/GridView.java
@@ -2,7 +2,7 @@
  *
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
  *
- *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
+ *  This code is Copyright 2009-2019 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
  *
  *  This file is part of DVB Inspector.
  *
@@ -46,6 +46,7 @@ import javax.swing.ScrollPaneConstants;
 
 import nl.digitalekabeltelevisie.controller.ViewContext;
 import nl.digitalekabeltelevisie.data.mpeg.TransportStream;
+import nl.digitalekabeltelevisie.gui.utils.WrapLayout;
 import nl.digitalekabeltelevisie.util.Utils;
 
 
@@ -68,7 +69,7 @@ public class GridView extends JPanel implements TransportStreamView{
 	public GridView(final TransportStream transportStream, final ViewContext viewContext) {
 		//super(new GridLayout(1,0));
 		super(new BorderLayout());
-		buttonPanel = new JPanel();
+		buttonPanel = new JPanel(new WrapLayout());
 
 		addPacketCheckBoxes();
 
