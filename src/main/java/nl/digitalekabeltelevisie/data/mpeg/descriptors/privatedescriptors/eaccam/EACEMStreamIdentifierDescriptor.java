1ea408d905cbb8c071bf0e3ac45f4d8267db1f2f
Eric Berendsen <github@digitalekabeltelevisie.nl>
2022-04-02 22:00:50 +0200

    type in missing getDescriptorname() for private descriptor

--- src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/privatedescriptors/eaccam/EACEMStreamIdentifierDescriptor.java
+++ src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/privatedescriptors/eaccam/EACEMStreamIdentifierDescriptor.java
@@ -2,7 +2,7 @@
  * 
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
  * 
- *  This code is Copyright 2009-2012 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
+ *  This code is Copyright 2009-2022 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
  * 
  *  This file is part of DVB Inspector.
  * 
@@ -57,7 +57,7 @@ public class EACEMStreamIdentifierDescriptor extends Descriptor {
 
 	@Override
 	public String getDescriptorname(){
-		return "Eacem Stream Identifier 2escriptor";
+		return "Eacem Stream Identifier Descriptor";
 	}
 
 
