121658ecce3f3e6639b4100f4aaf7276add4bddd
Eric Berendsen <github@digitalekabeltelevisie.nl>
2024-01-26 19:26:54 +0100

    fixed font_id in TtmlSubtitlingDescriptor

--- src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/extension/dvb/TtmlSubtitlingDescriptor.java
+++ src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/extension/dvb/TtmlSubtitlingDescriptor.java
@@ -2,7 +2,7 @@
  *
  *  http://www.digitalekabeltelevisie.nl/dvb_inspector
  *
- *  This code is Copyright 2009-2017 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
+ *  This code is Copyright 2009-2024 by Eric Berendsen (e_berendsen@digitalekabeltelevisie.nl)
  *
  *  This file is part of DVB Inspector.
  *
@@ -37,7 +37,6 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import nl.digitalekabeltelevisie.controller.DVBString;
 import nl.digitalekabeltelevisie.controller.KVP;
 import nl.digitalekabeltelevisie.controller.TreeNode;
-import nl.digitalekabeltelevisie.data.mpeg.descriptors.extension.dvb.TtmlSubtitlingDescriptor.Font;
 import nl.digitalekabeltelevisie.data.mpeg.psi.TableSection;
 import nl.digitalekabeltelevisie.util.LookUpList;
 import nl.digitalekabeltelevisie.util.Utils;
@@ -48,7 +47,7 @@ public class TtmlSubtitlingDescriptor extends DVBExtensionDescriptor {
 		
 		public Font(int font) {
 			super();
-			this.font_id = font&0x0111_1111;
+			this.font_id = font&0b0111_1111;
 		}
 
 		final int font_id;
