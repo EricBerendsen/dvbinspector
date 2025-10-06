c70694787c42deb50c5a1daa54b1443d3f794942
Stanislav Petkov <sunny@sunny78.net>
2025-10-06 19:06:38 +0300

    Update TimeDescriptor

--- src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/scte35/TimeDescriptor.java
+++ src/main/java/nl/digitalekabeltelevisie/data/mpeg/descriptors/scte35/TimeDescriptor.java
@@ -9,16 +9,13 @@ import static nl.digitalekabeltelevisie.util.Utils.*;
 
 public class TimeDescriptor extends SCTE35Descriptor {
 
-    private String identifier;
 	private long TAI_seconds;
     private long TAI_ns;
     private int UTC_offset;
 
 
 	public TimeDescriptor(byte[] b, int offset, TableSection parent) {
-		super(b, offset, parent);
-
-        identifier = getString(b,offset+2,4);
+		super(b, offset, parent);        
         TAI_seconds = getLong(b,offset+6,6, 0xFFFFFFFFFFFFL);
         TAI_ns = getLong(b,offset+12,4, 0xFFFFFFFFL);
         UTC_offset = getInt(b,offset+16, 2, 0xFFFF);
@@ -28,8 +25,7 @@ public class TimeDescriptor extends SCTE35Descriptor {
 	
 	@Override
 	public DefaultMutableTreeNode getJTreeNode(final int modus){
-		final DefaultMutableTreeNode t = super.getJTreeNode(modus);
-		t.add(new DefaultMutableTreeNode(new KVP("identifier",identifier ,null)));
+		final DefaultMutableTreeNode t = super.getJTreeNode(modus);		
 		t.add(new DefaultMutableTreeNode(new KVP("TAI_seconds",TAI_seconds ,null)));
 		t.add(new DefaultMutableTreeNode(new KVP("TAI_ns",TAI_ns ,null)));
 		t.add(new DefaultMutableTreeNode(new KVP("UTC_offset",UTC_offset ,null)));
